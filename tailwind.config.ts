49be4ba7f938e96a200aee339a1f003afa4b0ab2
JunYoung <whwnsdud011@naver.com>
2025-03-20 17:33:57 +0900

    chore: daisy ui 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,6 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+import type { Config } from "tailwindcss";
+
+const config: Config = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
@@ -16,3 +17,5 @@ export default {
   },
   plugins: [],
 };
+
+export default config;
