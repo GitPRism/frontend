92100501993f7e3cc51c7ac6f8746642c740a5c0
JunYoung <whwnsdud011@naver.com>
2025-03-20 20:42:51 +0900

    design: 온보딩 페이지 퍼블리싱

--- src/pages/Onboarding/index.tsx
+++ src/pages/Onboarding/index.tsx
@@ -1,5 +1,15 @@
+import Button from "./Button";
+
 function Onboarding() {
-  return <div className="text-amber-50">온보딩 페이지</div>;
+  return (
+    <div className="flex flex-col items-center justify-center h-screen gap-10 ">
+      <div className="text-center text-white text-3xl font-bold mb-6">
+        Github 프로젝트 분석 AI와 함께 쉽고 간편하게
+        <br /> <span className="text-sky-400">GitPRism</span>과 함께
+      </div>
+      <Button />
+    </div>
+  );
 }
 
 export default Onboarding;
