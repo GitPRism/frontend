9d21ed6e192d61c1a5903f0f6d54cab8f2660553
JunYoung <whwnsdud011@naver.com>
2025-05-11 16:52:14 +0900

    feat: 인기 순위 api 연동

--- src/components/SideBar/TrendingPortfolio.tsx
+++ src/components/SideBar/TrendingPortfolio.tsx
@@ -1,47 +1,45 @@
+import { useQuery } from "@tanstack/react-query";
+import apiClient from "@/services/apiClient";
+import { useNavigate } from "react-router-dom";
+
 function TrendingPortfolio() {
+  const navigate = useNavigate();
+  const { data, isLoading } = useQuery({
+    queryKey: ["trendingPortfolio"],
+    queryFn: () => apiClient.get("/api/v1/portfolios/popular?limit=10"),
+    refetchOnMount: false,
+    refetchOnWindowFocus: false,
+  });
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
   return (
-    <div className="py-2 px-1.5 text-center border-1 border-rank-border w-full">
+    <div className="py-2 px-1.5 text-center border-1 border-rank-border w-full rounded-lg">
       <p className="text-base pb-2 text-white">실시간 인기 순위</p>
 
       {/* 리스트 항목 */}
-      <div className="flex items-center border-b border-rank-divider p-1 gap-2">
-        <span className="w-4 shrink-0">1</span>
-        <span
-          className="flex-1 truncate text-rank-text"
-          title="이번에 합격한 포트폴리오"
-        >
-          이번에 합격한 포트폴리오
-        </span>
-        <span className="max-w-[80px] truncate text-rank-text" title="Uropa0-0">
-          Uropa0-0
-        </span>
-      </div>
-
-      <div className="flex items-center border-b border-rank-divider p-1 gap-2">
-        <span className="w-4 shrink-0">2</span>
-        <span className="flex-1 truncate text-rank-text" title="포트폴리오 V2">
-          포트폴리오 V2
-        </span>
-        <span className="max-w-[80px] truncate text-rank-text" title="Uropa">
-          Uropa
-        </span>
-      </div>
-
-      <div className="flex items-center border-b border-rank-divider p-1 gap-2">
-        <span className="w-4 shrink-0">3</span>
-        <span
-          className="flex-1 truncate text-rank-text"
-          title="포트폴리오 V2포트폴리오 V2포트폴리오 V2"
-        >
-          포트폴리오 V2포트폴리오 V2포트폴리오 V2
-        </span>
-        <span
-          className="max-w-[60px] truncate text-rank-text"
-          title="UropaUropaUropa"
-        >
-          UropaUropaUropa
-        </span>
-      </div>
+      <ul>
+        {data?.data.data.map((item: any, idx: number) => (
+          <li
+            key={item.portfolioId}
+            className="flex items-center border-b border-rank-divider p-1 gap-2 hover:bg-button-bg-second"
+            onClick={() => {
+              navigate(`/portfolio/${item.portfolioId}`);
+            }}
+          >
+            <div>
+              <span className="w-4 shrink-0 text-white">{idx + 1}</span>
+              <span className="flex-1 truncate text-rank-text">
+                {item.title}
+              </span>
+              <span className="max-w-[80px] truncate text-rank-text">
+                {item.author}
+              </span>
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
