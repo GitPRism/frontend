db5ecd9f426c10441dd049e80eff47d52a2c2609
JunYoung <whwnsdud011@naver.com>
2025-05-28 18:13:53 +0900

    feat: QueryClient에 기본 옵션 설정

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,16 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import "./index.css";
 import App from "./App.tsx";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false, // 탭 포커스시 재요청 안 함
+      refetchOnMount: false, // 컴포넌트 마운트시 재요청 안 함
+      refetchOnReconnect: false, // 네트워크 재연결시 재요청 안 함
+      retry: false, // 실패시 자동으로 재시도 안 함
+    },
+  },
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
